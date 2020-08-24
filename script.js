4861f4d8a36b6d4cc1b8848e4a15d0e86e4fafef
N O <rozenakraz@rambler.ru>
2020-08-24 13:13:59 +0300

    Complete 1st exercise finally

--- script.js
+++ script.js
@@ -45,6 +45,24 @@
 
 const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?'),
+      b = prompt('На сколько Вы его оцените?'),
+      c = prompt('Один из последних просмотренных фильмов?'),
+      d = prompt('На сколько Вы его оцените?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
 
 
 
