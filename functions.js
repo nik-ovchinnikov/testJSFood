2b6d842d77dc80acf6c2e5fb00e3d7d5e2f5eb05
N O <rozenakraz@rambler.ru>
2020-08-25 15:13:39 +0300

    Functions in use

---
+++ functions.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// let num = 30;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     alert(text);
+
+//     num = 120;
+//     alert(num);
+// }
+
+// num = 55;
+// alert(num);
+// showFirstMessage(num);
+// num = 57;
+// alert(num);
+
+// function calc (a, b) {
+//     return (a + b);
+// }
+
+
+// const logger = function() {
+//     console.log("Hello");
+// };
+
+const calc = (a, b) => a + b;
+
+console.log(calc(3, 116));
+console.log(calc(3, 28));
+console.log(calc(13, 26));
---
+++ numberStringAttr.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const str = "test";
+const arr = [1, 2, 4];
+
+console.log(arr.length);
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("ui"));
+
+const logg = "Hello world";
+
+console.log(logg.slice(-1));
+
+console.log(logg.slice(-1));
+
+const test = "12.2px";
+
+console.log(parseInt(test));
+
+
+console.log(parseFloat(test));
